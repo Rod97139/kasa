cf86b6c0ac75b4da740060c595c7ef98f055b14b
Rod97139 <fayaflame@gmail.com>
2024-04-09 01:17:53 +0200

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
+import DropDown from "../components/DropDown.jsx";
 
 const About = () => {
+
+    const dropdownData = [
+        {
+            title: "Fiabilité",
+            content:
+                "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+        },
+        {
+            title: "Respect",
+            content:
+                "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+        },
+        {
+            title: "Service",
+            content:
+                "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+        },
+        {
+            title: "Responsabilité",
+            content:
+                "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+        },
+    ];
+
+
 
 
 
     return (
         <main>
+            {dropdownData.map((dropdown, index) => (
+                <DropDown key={index} title={dropdown.title} content={dropdown.content} />
+            ))}
         <h1>About</h1>
         <p>Welcome to the About page</p>
         <Link to='/'>Home</Link>
