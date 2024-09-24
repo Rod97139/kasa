8767b1b1ccbfaeca7e7d4831525fd40e43a7a625
Rod97139 <fayaflame@gmail.com>
2024-09-24 22:24:09 +0200

    carrousel slide

--- src/components/pages/details/Carrousel.jsx
+++ src/components/pages/details/Carrousel.jsx
@@ -22,9 +22,24 @@ const Carrousel = ({ images }) => {
 
   return (
     <div className="carrousel">
-      <img className="carrousel-img" src={images[index]} alt="carrousel" />
-        <img className='carrousel-arrow-left' style={arrowShouldDisplay} src={arrowLeft} onClick={handlePrev} alt='arrow-left'/>
-        <img className='carrousel-arrow-right' style={arrowShouldDisplay} src={arrowRight} onClick={handleNext} alt='arrow-right'/>
+      <div className="carrousel-slide-container"
+      style={{transform: `translateX(-${index * 100}%)`}}
+      
+      >
+      {images.map((image, i) => (
+        <img
+          key={i}
+          className="carrousel-img"
+          src={image}
+          alt="carrousel"
+        />
+      ))}
+      </div>
+     
+      {/* <img className="carrousel-img" src={images[index]} alt="carrousel" /> */}
+
+      <img className='carrousel-arrow-left' style={arrowShouldDisplay} src={arrowLeft} onClick={handlePrev} alt='arrow-left'/>
+      <img className='carrousel-arrow-right' style={arrowShouldDisplay} src={arrowRight} onClick={handleNext} alt='arrow-right'/>
       <p className="carrousel-index">{index+1}/{images.length}</p>
     </div>
   );
